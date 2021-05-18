88fd690f18de41b70dd625b69b6d50dbcac0fbb0
Roman Sedaikin <rsedaikin@gmail.com>
2021-05-18 12:07:32 +0300

    Software rendering from TRANSLUCENT to OPAQUE.

--- skiko/src/jvmMain/kotlin/org/jetbrains/skiko/context/SoftwareContextHandler.kt
+++ skiko/src/jvmMain/kotlin/org/jetbrains/skiko/context/SoftwareContextHandler.kt
@@ -2,6 +2,7 @@ package org.jetbrains.skiko.context
 
 import org.jetbrains.skija.Bitmap
 import org.jetbrains.skija.Canvas
+import org.jetbrains.skija.ColorType
 import org.jetbrains.skija.ColorAlphaType
 import org.jetbrains.skija.ImageInfo
 import org.jetbrains.skija.Picture
@@ -14,20 +15,20 @@ import java.awt.image.DataBuffer
 import java.awt.image.DataBufferByte
 import java.awt.image.Raster
 import java.awt.image.WritableRaster
+import java.nio.ByteBuffer
 
 internal class SoftwareContextHandler(layer: SkiaLayer) : ContextHandler(layer) {
     override val bleachConstant = -1 // it looks like java.awt.Canvas doesn't support transparency
 
     val colorModel = ComponentColorModel(
         ColorSpace.getInstance(ColorSpace.CS_sRGB),
-        true,
         false,
-        Transparency.TRANSLUCENT,
+        false,
+        Transparency.OPAQUE,
         DataBuffer.TYPE_BYTE
     )
     val storage = Bitmap()
     var image: BufferedImage? = null
-    var imageData: ByteArray? = null
     var raster: WritableRaster? = null
     var isInited = false
 
@@ -50,7 +51,7 @@ internal class SoftwareContextHandler(layer: SkiaLayer) : ContextHandler(layer) 
         val h = (layer.height * scale).toInt().coerceAtLeast(0)
         
         if (storage.getWidth() != w || storage.getHeight() != h) {
-            storage.allocPixelsFlags(ImageInfo.makeS32(w, h, ColorAlphaType.PREMUL), false)
+            storage.allocPixelsFlags(ImageInfo(w, h, ColorType.RGB_888X, ColorAlphaType.UNPREMUL, org.jetbrains.skija.ColorSpace.getSRGB()), true)
         }
 
         canvas = Canvas(storage)
@@ -63,16 +64,14 @@ internal class SoftwareContextHandler(layer: SkiaLayer) : ContextHandler(layer) 
         val w = (layer.width * scale).toInt().coerceAtLeast(0)
         val h = (layer.height * scale).toInt().coerceAtLeast(0)
 
-
-        val bytes = storage.readPixels(storage.getImageInfo(), (w * 4).toLong(), 0, 0)
+        val bytes = storage.peekPixels()
         if (bytes != null) {
-            val buffer = DataBufferByte(bytes, bytes.size)
             raster = Raster.createInterleavedRaster(
-                buffer,
+                DirectDataBuffer(bytes),
                 w,
                 h,
                 w * 4, 4,
-                intArrayOf(2, 1, 0, 3), // BGRA order
+                intArrayOf(0, 1, 2), // BGRA order
                 null
             )
             image = BufferedImage(colorModel, raster!!, false, null)
@@ -82,5 +81,14 @@ internal class SoftwareContextHandler(layer: SkiaLayer) : ContextHandler(layer) 
 
     override fun flush() {
         // Raster does not need to flush canvas
+    }
+
+    class DirectDataBuffer(val backing: ByteBuffer): DataBuffer(TYPE_BYTE, backing.limit()) {
+        override fun getElem(bank: Int, index: Int): Int {
+            return backing[index].toInt()
+        }
+        override fun setElem(bank: Int, index: Int, value: Int) {
+            throw UnsupportedOperationException("no write access")
+        }
     }
 }
