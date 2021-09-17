896871f265e44b2828fcf59852c5dfb17f673a16
Nikolay Igotti <igotti@gmail.com>
2021-09-17 10:07:34 +0300

    JS rendering experiments

--- skiko/src/jsMain/cpp/wrapper.cc
+++ skiko/src/jsMain/cpp/wrapper.cc
@@ -1,6 +1,17 @@
+#include "common.h"
+
 #include "GrBackendSurface.h"
 #include "GrDirectContext.h"
+
+#include "SkSurface.h"
+
 #include <emscripten/bind.h>
+
+#include "gl/GrGLInterface.h"
+#include "gl/GrGLTypes.h"
+
+#include <GLES3/gl3.h>
+#include <emscripten/html5.h>
 
 using namespace emscripten;
 
@@ -16,3 +27,30 @@ EMSCRIPTEN_BINDINGS(Skiko) {
     function("ping", &ping);
 };
 
+sk_sp<SkSurface> MakeOnScreenGLSurface(sk_sp<GrDirectContext> grContext, int width, int height);
+
+SKIKO_EXPORT void* org_jetbrains_skia_MakeOnScreenGLSurface(sk_sp<GrDirectContext> grContext, int width, int height) {
+    return MakeOnScreenGLSurface(grContext, width, height).release();
+}
+
+#if 0
+sk_sp<SkSurface> MakeOnScreenGLSurface(sk_sp<GrDirectContext> grContext, int width, int height);
+
+SKIKO_EXPORT sk_sp<SkSurface> org_jetbrains_skia_MakeOnScreenGLSurface(sk_sp<GrDirectContext> grContext, int width, int height) {
+    return MakeOnScreenGLSurface(grContext, width, height);
+}
+
+SKIKO_EXPORT sk_sp<GrDirectContext> org_jetbrains_skia_MakeGrContext(EMSCRIPTEN_WEBGL_CONTEXT_HANDLE context) {
+    EMSCRIPTEN_RESULT r = emscripten_webgl_make_context_current(context);
+    if (r < 0) {
+        printf("failed to make webgl context current %d\n", r);
+        return nullptr;
+    }
+    // setup GrDirectContext
+    auto interface = GrGLMakeNativeInterface();
+    // setup contexts
+    sk_sp<GrDirectContext> dContext(GrDirectContext::MakeGL(interface));
+    return dContext;
+}
+
+#endif
--- skiko/src/jsMain/resources/setup.js
+++ skiko/src/jsMain/resources/setup.js
@@ -5,3 +5,56 @@ var wasmSetup = new Promise(function(resolve, reject) {
 });
 
 function onWasmReady(onReady) { wasmSetup.then(onReady); }
+
+function Debug(msg) {
+    console.warn(msg);
+}
+/** @const */ var IsDebug = true;
+
+const CanvasKit = {
+
+};
+
+CanvasKit.MakeWebGLCanvasSurface = function(idOrElement, colorSpace, attrs) {
+    colorSpace = colorSpace || null;
+    var canvas = idOrElement;
+    var isHTMLCanvas = typeof HTMLCanvasElement !== 'undefined' && canvas instanceof HTMLCanvasElement;
+    var isOffscreenCanvas = typeof OffscreenCanvas !== 'undefined' && canvas instanceof OffscreenCanvas;
+    if (!isHTMLCanvas && !isOffscreenCanvas) {
+        canvas = document.getElementById(idOrElement);
+        if (!canvas) {
+            throw 'Canvas with id ' + idOrElement + ' was not found';
+        }
+    }
+
+    var ctx = this.GetWebGLContext(canvas, attrs);
+    if (!ctx || ctx < 0) {
+        throw 'failed to create webgl context: err ' + ctx;
+    }
+
+    var grcontext = org_jetbrains_skia_MakeGrContext(ctx);
+
+    // Note that canvas.width/height here is used because it gives the size of the buffer we're
+    // rendering into. This may not be the same size the element is displayed on the page, which
+    // constrolled by css, and available in canvas.clientWidth/height.
+    var surface = org_jetbrains_skia_MakeOnScreenGLSurface(grcontext, canvas.width, canvas.height, colorSpace);
+    if (!surface) {
+        Debug('falling back from GPU implementation to a SW based one');
+        // we need to throw away the old canvas (which was locked to
+        // a webGL context) and create a new one so we can
+        var newCanvas = canvas.cloneNode(true);
+        var parent = canvas.parentNode;
+        parent.replaceChild(newCanvas, canvas);
+        // add a class so the user can detect that it was replaced.
+        newCanvas.classList.add('ck-replaced');
+
+        return org_jetbrains_skia_MakeSWCanvasSurface(newCanvas);
+    }
+    surface._context = ctx;
+    surface.grContext = grcontext;
+    surface.openGLversion = canvas.GLctxObject.version;
+    return surface;
+};
+
+// Default to trying WebGL first.
+CanvasKit.MakeCanvasSurface = CanvasKit.MakeWebGLCanvasSurface;
